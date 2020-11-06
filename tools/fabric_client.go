6c60eec363f035e36434e6141d9c168d8f1844d0
gaoyuan tan <chain_dev@163.com>
2020-11-06 19:20:02 +0800

    check validate of tx before commit to poly

--- tools/fabric_client.go
+++ tools/fabric_client.go
@@ -115,7 +115,7 @@ func (sdk *FabricSdk) GetCrossChainEvent(height uint64) ([]*CrossChainEvent, err
 				return nil, err
 			}
 			log.Infof("GetCrossChainEvent, height: %d, txid: %s, validate code: %d, event: %s\n", height, chaincodeEvent.TxId, txValidateCode, chaincodeEvent.EventName)
-			if strings.Contains(chaincodeEvent.EventName, "from_ccm") {
+			if txValidateCode == 0 && strings.Contains(chaincodeEvent.EventName, "from_ccm") {
 				txHash, _ := hex.DecodeString(chaincodeEvent.TxId)
 				events = append(events, &CrossChainEvent{
 					Data:   chaincodeEvent.Payload,
